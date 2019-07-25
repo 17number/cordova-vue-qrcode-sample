7b64199a268e65b3032f17759eaeb39356a38de0
r17n <4716989+17number@users.noreply.github.com>
2019-07-25 10:50:41 +0900

    chore: import vue-qrcode

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueQrcode from '@chenfengyuan/vue-qrcode';
+Vue.component(VueQrcode.name, VueQrcode);
 
 Vue.config.productionTip = false
 

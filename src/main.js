b32dea37b411caf391f6fcc7c1e4a8a9c9a7b3c7
r17n <4716989+17number@users.noreply.github.com>
2019-07-26 12:40:08 +0900

    chore: import vue-qrcode-reader

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueQrcode from '@chenfengyuan/vue-qrcode';
 Vue.component(VueQrcode.name, VueQrcode);
+import VueQrcodeReader from "vue-qrcode-reader";
+Vue.use(VueQrcodeReader);
 
 Vue.config.productionTip = false
 

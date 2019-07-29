076677cd95db96170e874637c4913781b28a416b
r17n <4716989+17number@users.noreply.github.com>
2019-07-29 10:04:43 +0900

    feat: QR reader with vue-qrcode-reader(camera available, but scan failed)

--- src/QrReader.vue
+++ src/QrReader.vue
@@ -1,3 +1,40 @@
+<!-- Notice!! This code is not work with cordova. -->
 <template>
-  <div>QR Reader</div>
+  <div>
+    <h2>Notice!! This code is not work with cordova.</h2>
+    <qrcode-capture @decode="onDecode" @detect="onDetect" />
+    <h2>Decoded content</h2>
+    <div>{{decodedString || "nothing"}}</div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "qrReader",
+  data () {
+    return {
+      decodedString: ""
+    }
+  },
+  methods: {
+    onDecode(decodedString) {
+      this.decodedString = decodedString;
+      console.debug({decodedString: decodedString});
+    },
+    async onDetect(promise) {
+      try {
+        const {
+          imageData,
+          content,
+          location
+        } = await promise;
+        console.debug({imageData: imageData, content: content, location: location});
+        this.decodedString = content || "nothing";
+      }
+      catch (error) {
+        console.error(error);
+      }
+    }
+  }
+};
+</script>
